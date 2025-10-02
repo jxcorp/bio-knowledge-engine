8b0e9204cdd69b605e91eb6e25bc5b9d0cdc3080
JeremiaXavier <jeremiaxavier2020@gmail.com>
2025-10-02 20:05:04 +0530

    first look is published

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 // vite.config.js
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react-swc';
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "url"; // Import fileURLToPath from 'url' module
@@ -12,14 +12,17 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/bio-knowledge-engine/`,
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       // Use the correctly defined __dirname for the alias resolution
       "@": path.resolve(__dirname, "./src"),
     },
-  }, 
-
+  },
+  build: {
+    outDir: "dist",
+  },
   // Ensures Vite treats CSV files as simple assets (URLs) for fetching
-  assetsInclude: ['**/*.csv'], 
+  assetsInclude: ["**/*.csv"],
 });
